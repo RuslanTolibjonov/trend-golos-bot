5b578b65e883689d25640f6229f270841315d83a
Ruslan Tolibjonov <tolibjonovruslan90@gmail.com>
2026-05-31 15:46:16 +0300

    fix token

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 import asyncio
-import os
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
-from dotenv import load_dotenv
 
-load_dotenv()
+BOT_TOKEN = "8819485644:AAHg6E4ITngFDKeQCL4EfTAy5em5dNk8TOI"
 
-BOT_TOKEN = os.getenv("8819485644:AAHg6E4ITngFDKeQCL4EfTAy5em5dNk8TOI")
-
-bot = Bot(BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: Message):
-    await message.answer(
-        "🔥 Trend Golos Bot ishga tushdi!\n\n"
-        "Tez orada trend ovozlar qo'shiladi."
-    )
+    await message.answer("🔥 Bot ishladi!")
 
 async def main():
+    print("Bot started")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-
-# redeploy
